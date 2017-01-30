04bb62c9b6cb8acbfc0b3bde39868f12bf4121a9
Harshvardhan Poddar <harsha.poddar@yahoo.com>
2017-01-31 00:05:53 +0530

    Checked and Completed.

--- HmsAccess/HmsAccess/Program.cs
+++ HmsAccess/HmsAccess/Program.cs
@@ -11,33 +11,33 @@ namespace HmsAccess
         static void Main(string[] args)
         {
             var ctx = new HotelManagementServerEntities();
-          
+        
             //ADD GUESTS
             Console.WriteLine("Enter the Name of the Guest you want to add : ");
             string name = Console.ReadLine();
-            var g1 = new Guest() { Name = name };
-            ctx.Guests.Add(g1);
+            var guest = new Guest() { Name = name };
+            ctx.Guests.Add(guest);
 
             //REMOVE GUESTS
             Console.WriteLine("Enter the GuesID you want to delete? : ");
-            int x = Convert.ToInt16(Console.ReadLine());
-            var remove = from id in ctx.Guests
-                         where id.GuestId == x
-                         select id;
-            foreach (var value in remove)
-                ctx.Guests.Remove(value);
+            int id = Convert.ToInt16(Console.ReadLine());
+            var removeGuest = from names in ctx.Guests
+                         where names.GuestId == id
+                         select names;
+            foreach (var Name in removeGuest)
+                ctx.Guests.Remove(Name);
                 
 
 
             //INSERT BOOKING
             Console.WriteLine("Enter the GuestID:");
-            int gid = Convert.ToInt16(Console.ReadLine());
+            int guestid = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("Enter the Check In Date:");
-            DateTime cin = Convert.ToDateTime(Console.ReadLine());
+            DateTime checkinDate = Convert.ToDateTime(Console.ReadLine());
             Console.WriteLine("Enter the Check Out Date:");
-            DateTime cout = Convert.ToDateTime(Console.ReadLine());
+            DateTime checkoutDate = Convert.ToDateTime(Console.ReadLine());
 
-            var insert = new Booking() { GuestID = gid, CheckInDate = cin, CheckOutDate = cout };
+            var insert = new Booking() { GuestID = guestid, CheckInDate = checkinDate, CheckOutDate = checkoutDate };
             ctx.Bookings.Add(insert);
             
 
@@ -63,20 +63,20 @@ namespace HmsAccess
                 DateTime []  days = new DateTime[31];
                 j++;
                 i = 0;
-                for (DateTime y = DateTime.Now; y <= test; y = y.AddDays(1))
+                for (DateTime DateCheck = DateTime.Now; DateCheck <= test; DateCheck = DateCheck.AddDays(1))
                 {
-                    foreach (var val in month)
+                    foreach (var day in month)
                     {
 
-                        if (val.Room.RoomId == j)
+                        if (day.Room.RoomId == j)
                         {
-                            if (y.CompareTo(val.CheckInDate) > 0 && y.CompareTo(val.CheckOutDate) < 0)
-                                y = y.AddDays(1);
+                            if (DateCheck.CompareTo(day.CheckInDate) > 0 && DateCheck.CompareTo(day.CheckOutDate) < 0)
+                                DateCheck = DateCheck.AddDays(1);
                             else
                                 break;
                         }
                         else
-                            days[i] = y;
+                            days[i] = DateCheck;
                     }
                     i++;
                 }
@@ -92,9 +92,11 @@ namespace HmsAccess
                 
             }
 
-            ctx.SaveChanges();
 
             Console.ReadKey();
+
+            ctx.SaveChanges();
         }
+
     }
 }
